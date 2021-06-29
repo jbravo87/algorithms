a87e4fccfda39c972cb6dff282598631ce3bca1e
J.Bravo <joepbravo@gmail.com>
2021-06-28 23:13:20 -0700

    Finished algorithm that multiplies two large numbers.

--- ancient_egyptian_multiplication.jl
+++ ancient_egyptian_multiplication.jl
@@ -1,27 +1,40 @@
-n1 = 89
-n2 = 18
-# Will create two vectors.
-# One handling halving and doubling
-halving = [n1]
-# Remember that Julia is 1-based indexing.
-i = 1
-while halving[i] > 1
-    push!(halving, floor(halving[i] / 2))
-    i += 1
-end
-
-j = 1
-doubling = [n2]
-while length(doubling) < length(halving)
-    push!(doubling, doubling[j] * 2)
-    j += 1
-end
-
+# import necessary libraries
 using DataFrames
-# Will join the two tables created earlier.
-half_double = hcat(halving, doubling)
-# Used horizontal concatenating instead of DataFrame
 
-# The following will give the location of the odd number in the data table.
-findall(isodd, half_double)
-findall(x -> x % 2 == 1, half_double)
+# Will use the alternate name for this historic algorithm.
+# Russian peasant multiplication (rpm).
+# Will take two arguments.
+function rpm(n1=89, n2=18)
+	# One argument will be halved and the other doubled.
+	halving = [n1]
+	# Remember that Julia is 1-based indexing.
+	i = 1
+	while halving[i] > 1
+		push!( halving, floor( halving[i]/2 ) )
+		i += 1
+	end
+
+	# Now to handle the doubling.
+	doubling = [n2]
+	j = 1
+	while length( doubling ) < length( halving )
+		push!( doubling, doubling[j]*2 )
+		j += 1
+	end
+
+	# Now convert the vectors into dataframes.
+	df1 = DataFrame(half=halving)
+	df2 = DataFrame(double=doubling)
+
+	# To join the two different DataFrames
+	# Also want to assign it to a new variable
+	df3 = hcat(df1, df2)
+
+	# Next line will multiply all elements in column A and give those not equal to ten
+	df4 = filter(x -> x.half .%2 == 1, df3)
+	# Will return a new data frame where the rows where the 'half' column is odd.
+
+	answer = sum(df4.double)
+
+	return println("The product of $n1 and $n2 is $answer")
+end
