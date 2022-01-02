2e35e6540333ac4e2606b5750c7dc7893587f137
Jose Bravo <joepbravo@gmail.com>
2022-01-01 21:35:15 -0800

    Bundled logic into a function with test case.

--- binary_search.jl
+++ binary_search.jl
@@ -1,21 +1,29 @@
 # Binary Search Algorithm
-sorted_array = [1, 2, 3, 4, 5]
-supremum = length(sorted_array)
-infimum = 0
-
-# Guess veriable cuts in half the some_array
-guess = convert(Int64, floor(supremum))
-
-# The variable will store the number we are looking for.
-seeking = 4
-if sorted_array[guess] < seeking
-    infimum = convert(Int64, guess)
-    guess = convert(Int64, floor((guess + supremum)/2))
+sortedarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sortedarray2 = [34, 35, 36, 37, 38, 39, 40]
+function binarysearch(array, seeking)
+    # Guess veriable cuts in half the some_array
+    guess = floor(length(array)/2)
+    guess = convert(Int64, guess)
+    supremum = length(array)
+    infimum = 0
+    while(abs(array[guess] - seeking) > 0.0001)
+        if array[guess] > seeking
+            supremum = guess
+            guess = floor((guess + infimum)/2)
+            guess = convert(Int64, guess)
+        end
+        if array[guess] < seeking
+            infimum = guess
+            guess = floor((guess + supremum)/2)
+            guess = convert(Int64, guess)
+        end
+    end
+    return(guess)
 end
 
-if sorted_array[guess] > seeking
-    supremum = convert(Int64, guess)
-    guess = convert(Int64, floor((guess + infimum)/2))
-end
+# Below creates infinite loop because evaluating an array where the 'seeking'
+# does not exits, hence the while loop will evaluate forever.
+#println(binarysearch(sortedarray, 38))
 
-println("The guess is: ", guess)
+println("The number 38 in the array is in position: ", binarysearch(sortedarray2, 38))
