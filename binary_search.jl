0afe69a423b530d4d1049e41c6b3d9737f119679
Jose Bravo <joepbravo@gmail.com>
2022-01-03 21:45:44 -0800

    Used binary search to create inverse trig function.

--- binary_search.jl
+++ binary_search.jl
@@ -27,3 +27,41 @@ end
 #println(binarysearch(sortedarray, 38))
 
 println("The number 38 in the array is in position: ", binarysearch(sortedarray2, 38))
+
+#=
+    Will use binary search to invert the cosine function.
+    Call the binary search to get correct answer.
+=#
+
+function arccos(number)
+    an_array = [j for j in 1:10000]
+    domain = [pi/2 - i*(pi/10000) for i in an_array]
+    codomain = [cos(x) for x in domain]
+    result = domain[binarysearch(codomain, number)]
+    return result
+end
+
+println("\nThe inverse cosine of 0.9 is: ", arccos(0.9))
+
+# Now want to plot the function if indeed looks like inverse cosine
+
+#x = []
+#for i = 1
+a, b, c = (-pi/2), (pi/2), 10000
+n = (b - a)/(c - 1)
+x = collect(a:n:b)
+
+y = [arccos(i) for i in x]
+
+x4 = []
+for j = -1:0.0001:1
+    push!(x4, j)
+end
+
+y4 = []
+for i in x4
+    push!(arccos(x4), y4)
+end
+
+y4 = [arccos(i) for i in x4]
+arccos(-1.0)
