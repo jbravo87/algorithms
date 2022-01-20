1d99c1540c17f3c72ae686ff424f1f3f2b148e88
Jose Bravo <joepbravo@gmail.com>
2022-01-19 22:22:18 -0800

    Added a PNG file as well as finished the LCG.

--- linear_congruential_generator.jl
+++ linear_congruential_generator.jl
@@ -2,6 +2,9 @@
 A linear congruential generator (LCR) algorithm
 =#
 
+# Necessary libraries
+using Plots
+
 function next_random(previous, n1, n2, n3)
     next = (previous * n1 + n2) % n3
     return next
@@ -25,3 +28,23 @@ end
 #print(list_randoms(1, 18, 36))
 
 print(list_randoms(1, 1, 37))
+
+function overlapping_sums_test(some_list, sum_length)
+    list_length = length(some_list)
+    append!(some_list, some_list) # Recall flow of logic. This must be in the correct slot.
+    output = []
+    for k in 1:list_length
+        push!(output, sum(some_list[k:(k + sum_length)]))
+    end
+    
+    return output
+
+    
+end
+
+overlap = overlapping_sums_test(list_randoms(211111, 111112, 300007), 12)
+
+histogram(overlap, color=:green, legend = false)
+title!("Overlapping Sums Test")
+xlabel!("sum of elements of overlapping consecutive list sections")
+ylabel!("sum frequency")
