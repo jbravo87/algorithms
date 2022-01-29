2d6c82258440655bef6e56343a5618e8671f3aa4
Jose Bravo <joepbravo@gmail.com>
2022-01-28 23:07:19 -0800

    New functions to generate lines and compute displacement.

--- tsp.jl
+++ tsp.jl
@@ -11,7 +11,7 @@ using Random
 # Random Number Generator -> rng
 rng = Random.seed!(1729)
 
-N = 40
+N = 41
 
 # A random permutation command experimented with
 #randperm(rng, N)
@@ -45,6 +45,42 @@ end
 
 points2
 
-for k in 1:39
-    push!(lines, ([cities[itinerary[k]], cities[itinerary[k+1]]]))
+#for k in 1:N-1
+#    push!(lines, ([cities[k, k], cities[k, 2]]))
+#end
+
+lines2 = []
+for j in 1:N-1
+    push!(lines2, [[x[j], y[j]], [x[j + 1], y[j + 1]]])
+end
+
+lines3 = []
+for k in 1:N - 1
+    push!(lines3, [[cities[k, 1], cities[k, 2]], [cities[k+1, 1], cities[k+1, 2]]])
+end
+
+# Will calculate Euclidean Distance
+euclid_dist = sqrt(abs(lines3[1][2][1] - lines3[1][1][1])^2 + abs(lines3[1][2][2] - lines3[1][1][2])^2)
+println("The euclidean distance is: ", euclid_dist)
+
+lines4 = []
+for i in 1:N-1
+    push!(lines4, [[cities[i, 1], cities[i, 2]], [cities[i+1, 1], cities[i+1, 2]]])
+end
+
+# For number of rows of a given array
+size(cities, 1)
+# For number of columns of a given array
+size(cities, 2)
+
+# Now want to bundle into a function.
+# Will take two arguements.
+# An N x 2 matrix for x and y coordinates, and N <- number of cities
+function create_lines(cities)
+    n = size(cities, 1) - 1
+    lines = []
+    for k in 1:n
+        push!(lines, [[cities[k, 1], cities[k, 2]], [cities[k+1, 1], cities[k+1, 2]]])
+    end
+    return lines
 end
