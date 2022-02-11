f5923250e4a26e96d8b0ac3c6315d23342dec4a9
Jose Bravo <joepbravo@gmail.com>
2022-02-10 22:49:42 -0800

    Adding evolutionary stages of script through various functions.

--- codebreakingalgo.jl
+++ codebreakingalgo.jl
@@ -44,3 +44,62 @@ for j in 1:populationsize
     push!(population, chromosomes)
 end
 print(population)
+#=
+Need to set-up function for the various evolutionary stages including:
+1. Fitness Scoring
+2. Selecting parents
+3. Breeding to create new children
+4. Mutation
+=#
+# Fitness Scoring
+function fitness(population)
+    fitnessscores = []
+    for chromosome in population
+        matches = 0
+        for index in 1:codelength
+            if secretpasscode[index] == chromosome[index]
+                matches += 1
+            end
+        end
+        result = [chromosome, matches]
+        push!(fitnessscores, result)
+    end
+    return fitnessscores
+end
+
+#Selecting Parents
+function selectparents(fitnessscores)
+    parentslist = []
+    sortedlist = sort(fitnessscores, by = x -> x[2], rev=true)
+    for chromosome in sortedlist
+        push!(parentslist, chromosome[1])
+    end
+    return parentslist
+end
+#=
+Breeding to create new Children.
+1st function is breeding logic. Two parents have randomly selected start and end cut point position.
+Positions are used to combine the Genes of two parents to create new Child.
+Second function used to select parents who make it to breeding to create enough children for a new
+population. Elitism/favorable incorporated here.
+=#
+# First, breeding logic
+function breed(oarent1, parent2)
+    child = []
+    parent1 = parents[1]
+    parent2 = parents[2]
+    genea = Int(round(rand()*codelength))
+    geneb = Int(round(rand()*codelength))
+    startgene = min(genea, geneb)
+    endgene = max(genea, geneb)
+    for i in 1:codelength
+        if i < startgene || i > endgene
+            push!(child, parent1[i])
+        else
+            push!(child, parent2[i])
+        end
+    end
+    return child
+end
+
+
